84ca9cc1ac6d61191d2f7451f76a8428bbd9a3f7
Bryan Hanks, PMP, CSM <bryan.hanks@slalom.com>
2021-09-02 17:16:13 -0500

    Removed commented-out code

--- note_to_markdown.py
+++ note_to_markdown.py
@@ -56,26 +56,3 @@ class Note2mdCommand(sublime_plugin.TextCommand):
         firstLineRegion = self.view.line(point)
         firstLine = self.view.substr(firstLineRegion)
         self.view.insert(edit, point, filenameify(firstLine))
-
-        # cursor = view.sel()[0].a
-        # line_start = cur_line.a
-
-        # line1 = sublime.Region(0,0)
-        # line2 = sublime.Region(1,0)
-        # allstuff = sublime.Region(0, self.view.size())
-        # myline1 = self.view.line(line1)
-        # myline2 = self.view.line(line2)
-        # tp = self.view.text_point(0, 0)
-        # myline1 = self.view.rowcol( tp )
-        # mylineo = text_point(1, 1, True)
-
-        # myline2 = self.view.rowcol(2, 0)
-
-        # self.view.insert(edit, myline2, today.strftime("%Y%m%d @ %H:%M"))
-        # self.view.insert(edit, 0, today.strftime("%Y%m%d @ %H:%M\n"))
-
-        # p = re.compile("\s*#?\s*(.*)")
-
-        # juicy = myline
-        # self.view.replace(edit, myline, 'Cheese and crackers!')
-        # self.view.insert(edit, 0, "Hello, World!")
