415126253a1c8982143e3c35cecfa802965b3459
Bryan Hanks, PMP, CSM <bryan.hanks@slalom.com>
2021-09-02 17:15:29 -0500

    Correctly parses the first line and strips punctuation

--- note_to_markdown.py
+++ note_to_markdown.py
@@ -2,8 +2,24 @@ import re
 import sublime
 import sublime_plugin
 
-# from datetime import date
 from datetime import datetime
+# from datetime import date
+
+# note_to_markdown.py
+#
+# Subline Text plugin to convert raw notes in markdown format
+# (but classified as text):
+#    extract the document title (first header)
+#    lower-case & replace spaces with underscores
+#    prefix with timestamp
+#    suffix it with markdown extension
+# Open save dialog with the filename as generated above
+#
+# TODO: date format from preferences
+# TODO: word delimiter from preferences
+#
+# Check the validity of this script by:
+# flake8 note_to_markdown.py
 
 
 def next_line(view, pt):
@@ -16,13 +32,17 @@ def prev_line(view, pt):
 
 def filenameify(title):
     todays = datetime.now()
+    nopunct = re.compile(r'[\"\'\\\/\$\%\#\@=+\^]+')
     nospace = re.compile(r'\s+')
 
+    # TODO - use preferences for space replacement
     fixed = nospace.sub("_", title.lower().strip(' #\t\n\r'))
-
+    # TODO - use preferences for punctuation replacement
+    fixed1 = nopunct.sub('', fixed)
     # TODO - use preferences for suffix string
     # TODO - use preferences for date/time string
-    return "foo" #todays.strftime("%Y%m%d_%H%M-") + fixed + ".md_bonus" + "\n"
+    return todays.strftime("%Y%m%d_%H%M-") + fixed1 + ".md" + "\n"
+
 
 class Note2mdCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -30,11 +50,12 @@ class Note2mdCommand(sublime_plugin.TextCommand):
         sublime.log_result_regex(True)
         sublime.log_commands(True)
 
-        topline = "the quick brown fox"
-
-        # view = self.view
-        point = self.view.text_point(1, 0)  # second line point
-        self.view.insert(edit, point, filenameify(topline) )
+        firstLine = "# The Quick Brown Fox"
+        # Get the contents of the first line
+        point = self.view.text_point(0, 0)
+        firstLineRegion = self.view.line(point)
+        firstLine = self.view.substr(firstLineRegion)
+        self.view.insert(edit, point, filenameify(firstLine))
 
         # cursor = view.sel()[0].a
         # line_start = cur_line.a
